355523540d6598ac280b5b8b587d1cc2ef1cb48f
Elie AMAR <eamar@bess-f4r5s4.clusters.42paris.fr>
2024-04-03 18:52:29 +0200

    ft_strdup: sucessfully call extern ft_strlen, calling extern malloc seems to work but then segfault

--- ft_strdup/main.c
+++ ft_strdup/main.c
@@ -1,6 +1,29 @@
-extern char *strdup(const char *s);
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
-int	main(void)
+extern char *ft_strdup(const char *s);
+
+int	main(int argc, char **argv)
 {
-	return 0
+	//char *src = strdup();
+	(void)argc;(void)argv;
+
+	char *str = ft_strdup("test");
+	//printf("str = [%s]\n", str);
+	free(str);
+	return 0;
+
+	//char *str = ft_strdup(argv[1]);
+	//
+	//if (!str)
+	//{
+	//	dprintf(2, "Allocation failed\n");
+	//	perror("perror");
+	//	return -1;
+	//}
+	//
+	//printf("str = [%s]\n", str);
+	//free(str);
+	//return 0;
 }
