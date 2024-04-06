7aaa3c52ec77350d213d587c92d86ca79e7999a5
Elie AMAR <eamar@bess-f4r2s4.clusters.42paris.fr>
2024-04-06 18:29:24 +0200

    ft_strdup.s ++

--- ft_strdup/ft_strdup.s
+++ ft_strdup/ft_strdup.s
@@ -1,10 +1,35 @@
 global ft_strdup
+
 extern malloc
+
 section .text
 
+extern ft_strlen
+extern ft_strcpy
+
+;rdi contained s
 
 ft_strdup:
 
-mov rdi, 10
-call malloc wrt ..plt
+;mov rsi, rdi ;save the string pointer for later to make rdi available
+mov r8, rdi ;save the string pointer for later to make rdi available
+
+call ft_strlen
+
+mov rdi, rax ;prepare rdi for malloc call with desired size (from strlen)
+;add rdi, 100 ;for '\0'
+add rdi, 1 ;for '\0'
+
+call malloc wrt ..plt ;modifie les valeurs des registres ...
+
+
+mov byte [rax], 'A'
+mov byte [rax + 1], 0
+
+mov rsi, r8
+mov rdi, rax
+
+call ft_strcpy
+
+
 ret
