181ea56a56f1f3e8db08d0aade3290ae7fba5f10
Your Name <test@example.com>
2024-04-07 17:03:25 +0200

    strdup++

--- ft_strdup/ft_strdup.s
+++ ft_strdup/ft_strdup.s
@@ -11,8 +11,8 @@ extern ft_strcpy
 
 ft_strdup:
 
-;mov rsi, rdi ;save the string pointer for later to make rdi available
-mov r15, rdi ;save the string pointer for later to make rdi available
+mov r15, rdi ;save the string pointer for later to make rdi available, I chose to save rdi in a non volatile register rather then using push and pop for optimization purposes
+; (alternatively) push rdi
 
 call ft_strlen
 
@@ -22,10 +22,13 @@ add rdi, 1 ;for '\0'
 
 
 call malloc wrt ..plt ; carefull of malloc modifying registers ...
+;verify malloc return
 
 mov byte [rax], 0 ;avoid valgrind : conditional jump on non initialized value (during ft_strcpy call)
 
 mov rsi, r15
+; (alternatively) pop rsi
+
 mov rdi, rax
 
 call ft_strcpy
